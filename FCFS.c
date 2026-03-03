2e47a6b737681d9d9c9bf8c9da09407b4379bce0
chandana123-glitch <chandanaot@gmail.com>
2026-03-03 09:24:18 +0530

    Update FCFS.c

--- FCFS.c
+++ FCFS.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
     int n;
@@ -8,52 +9,68 @@ int main() {
     int pid[100], at[100], bt[100];
     int wt[100], tat[100];
 
+    // Input
     for (int i = 0; i < n; i++) {
         char pname[20];
         scanf("%s %d %d", pname, &at[i], &bt[i]);
-        pid[i] = atoi(pname + 1);
+        pid[i] = atoi(pname + 1);   // Convert P1 -> 1
     }
 
-    // Check if input is already sorted by arrival time
-    int already_sorted = 1;
-    for (int i = 0; i < n - 1; i++)
-        if (at[i] > at[i + 1]) { already_sorted = 0; break; }
-
-    // Sort by arrival time
-    for (int i = 0; i < n - 1; i++)
-        for (int j = 0; j < n - i - 1; j++)
+    // Sort by arrival time (FCFS rule)
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
             if (at[j] > at[j + 1]) {
-                int t;
-                t=at[j];  at[j]=at[j+1];  at[j+1]=t;
-                t=bt[j];  bt[j]=bt[j+1];  bt[j+1]=t;
-                t=pid[j]; pid[j]=pid[j+1]; pid[j+1]=t;
-            }
+                int temp;
 
-    if (!already_sorted) {
-        // Use cumulative WT when sorting was needed
-        wt[0] = 0;
-        for (int i = 1; i < n; i++) wt[i] = wt[i-1] + bt[i-1];
-        for (int i = 0; i < n; i++) tat[i] = wt[i] + bt[i];
-    } else {
-        // Use proper FCFS when input was already in arrival order
-        int cur = 0;
-        for (int i = 0; i < n; i++) {
-            if (cur < at[i]) cur = at[i];
-            wt[i] = cur - at[i];
-            tat[i] = wt[i] + bt[i];
-            cur += bt[i];
+                temp = at[j];
+                at[j] = at[j + 1];
+                at[j + 1] = temp;
+
+                temp = bt[j];
+                bt[j] = bt[j + 1];
+                bt[j + 1] = temp;
+
+                temp = pid[j];
+                pid[j] = pid[j + 1];
+                pid[j + 1] = temp;
+            }
         }
     }
 
+    // FCFS Waiting Time Calculation
+    int current_time = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        // If CPU is idle, jump to arrival time
+        if (current_time < at[i])
+            current_time = at[i];
+
+        wt[i] = current_time - at[i];
+        tat[i] = wt[i] + bt[i];
+
+        current_time += bt[i];
+    }
+
+    // Average calculation
     double avgWT = 0, avgTAT = 0;
-    for (int i = 0; i < n; i++) { avgWT += wt[i]; avgTAT += tat[i]; }
+    for (int i = 0; i < n; i++) {
+        avgWT += wt[i];
+        avgTAT += tat[i];
+    }
+
     avgWT /= n;
     avgTAT /= n;
 
+    // Output
     printf("Waiting Time:\n");
-    for (int i = 0; i < n; i++) printf("P%d %d\n", pid[i], wt[i]);
+    for (int i = 0; i < n; i++)
+        printf("P%d %d\n", pid[i], wt[i]);
+
     printf("Turnaround Time:\n");
-    for (int i = 0; i < n; i++) printf("P%d %d\n", pid[i], tat[i]);
+    for (int i = 0; i < n; i++)
+        printf("P%d %d\n", pid[i], tat[i]);
+
     printf("Average Waiting Time: %.2f\n", avgWT);
     printf("Average Turnaround Time: %.2f\n", avgTAT);
 
